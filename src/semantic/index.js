f63bad828976438a14cc3c7c1195f62cbf27ab60
martcomadm <martcomadm@gmail.com>
2026-08-18 15:11:06 -0600

    Add files via upload

---
+++ src/semantic/index.js
@@ -0,0 +1,4 @@
+export { normalizeEmployment, normalizeImss, normalizeSemantic, semanticEquivalent } from "./normalizer.js";
+export { validateNameCandidate, extractValidatedName, titleCaseName } from "./name-validator.js";
+export { resolveAnswer } from "./answer-resolver.js";
+export { detectFrustration } from "./frustration.js";
