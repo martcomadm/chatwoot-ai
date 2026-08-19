b87f4829511756b70a75492f83ba703e492795f6
martcomadm <martcomadm@gmail.com>
2026-08-19 10:11:26 -0600

    Delete src directory

--- src/semantic/index.js
+++
@@ -1,6 +0,0 @@
-export { normalizeEmployment, normalizeImss, normalizeSemantic, semanticEquivalent } from "./normalizer.js";
-export { validateNameCandidate, extractValidatedName, titleCaseName } from "./name-validator.js";
-export { resolveAnswer } from "./answer-resolver.js";
-export { detectFrustration } from "./frustration.js";
-
-export { resolveNegationScope } from "./negation-scope-resolver.js";
