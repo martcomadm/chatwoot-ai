e9510b2119f67f981cf51de7b9c9debadc333665
martcomadm <martcomadm@gmail.com>
2026-08-19 10:13:06 -0600

    Add files via upload

---
+++ src/handoff/index.js
@@ -0,0 +1,4 @@
+export { HandoffRotationStore } from "./handoff-rotation-store.js";
+export { HandoffRouter } from "./handoff-router.js";
+
+export { OperationsConfigStore } from "./operations-config-store.js";
---
+++ src/semantic/index.js
@@ -0,0 +1,6 @@
+export { normalizeEmployment, normalizeImss, normalizeSemantic, semanticEquivalent } from "./normalizer.js";
+export { validateNameCandidate, extractValidatedName, titleCaseName } from "./name-validator.js";
+export { resolveAnswer } from "./answer-resolver.js";
+export { detectFrustration } from "./frustration.js";
+
+export { resolveNegationScope } from "./negation-scope-resolver.js";
